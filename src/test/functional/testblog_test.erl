a5f74f316e997944f2929b1fb2a034fd79f550da
Rodrigo Martins <rmjorge@rmjorge.(none)>
2014-04-02 20:32:36 -0300

    first commit

---
+++ src/test/functional/testblog_test.erl
@@ -0,0 +1,7 @@
+-module(testblog_test).
+-compile(export_all).
+
+start() ->
+  boss_web_test:get_request("greeting/hello", [], 
+                            [ fun boss_assert:http_ok/1, 
+                                 fun(Res) -> boss_assert:tag_with_text("strong", "Rahm says hello!", Res) end], []).
